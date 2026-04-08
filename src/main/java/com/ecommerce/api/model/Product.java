347b38c494cc3a9897daee3ee46da3840497de89
Yash Advani <54671322+yash976@users.noreply.github.com>
2026-04-08 14:46:47 +0530

    Create Product.java
    Add Product model

---
+++ src/main/java/com/ecommerce/api/model/Product.java
@@ -0,0 +1,28 @@
+package com.ecommerce.api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private int stock;
+}
