bac7468aac21abed16c74f5e758c14737760b798
Yash Advani <54671322+yash976@users.noreply.github.com>
2026-04-08 14:46:10 +0530

    Create User.java
    Add User entity with JPA mapping

---
+++ src/main/java/com/ecommerce/api/model/User.java
@@ -0,0 +1,30 @@
+package com.ecommerce.api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders;
+}
